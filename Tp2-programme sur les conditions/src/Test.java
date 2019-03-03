2557942561bbf709f16f98f8072cc3f2ac625eb7
hello <hello@redmush>
2019-03-03 20:44:38 +0100

    Hello world

---
+++ Tp2-programme sur les conditions/src/Test.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Test {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        
+        System.out.print("Entrez un nombre entier:");
+        int number = scanner.nextInt();
+
+        if (number == 0) {
+            System.out.println("Le nombre est zéro (et il est pair)");
+        } else {
+
+            if (number < 0) {
+            	System.out.print("Le nombre est négatif ");
+            	} 		else {
+                System.out.print("Le nombre est positif ");
+            }
+
+            int reste = number % 2;
+            if (reste == 0) {
+                System.out.println("et pair");
+            } else {
+                System.out.println("et impair");
+            }
+        }
+    }
+}
